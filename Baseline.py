973540966caf118acbad672b33d9cf1e79616a9c
Ella_Zhang <74286996+ellazhang-gif@users.noreply.github.com>
2022-10-30 23:21:50 +0800

    Update Baseline.py

--- Baseline.py
+++ Baseline.py
@@ -157,6 +157,25 @@ class CovidCTDataset(Dataset):
         return sample
 
 
+# label smoothing loss function
+def reduce_loss(loss, reduction='mean'):
+    return loss.mean() if reduction=='mean' else loss.sum() if reduction=='sum' else loss
+
+
+class LabelSmoothingCrossEntropy(nn.Module):
+    def __init__(self, epsilon:float=0.1, reduction='mean'):
+        super().__init__()
+        self.epsilon = epsilon
+        self.reduction = reduction
+    
+    def forward(self, preds, target):
+        n = preds.size()[-1]
+        log_preds = F.log_softmax(preds, dim=-1)
+        loss = reduce_loss(-log_preds.sum(dim=-1), self.reduction)
+        nll = F.nll_loss(log_preds, target, reduction=self.reduction)
+        return linear_combination(loss/n, nll, self.epsilon)
+
+
 
     
 if __name__ == '__main__':
